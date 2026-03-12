a9b7db3942dea012060574562260ba5468cbffa0
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2026-03-12 17:13:56 -0300

    segundo commit rcv-daspu

--- rcv_Backend/src/main/java/com/backend/rcv/config/WebConfig.java
+++ rcv_Backend/src/main/java/com/backend/rcv/config/WebConfig.java
@@ -10,12 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                // Agregamos la IP de tu compañero y permitimos localhost por si hace pruebas locales
-                .allowedOriginPatterns(
+                .allowedOrigins(
                         "https://rcv-daspu.vercel.app",
-                        "http://181.96.178.138*",
-                        "http://localhost:*",
-                        "https://*.vercel.app"
+                        "http://181.96.178.138", // IP sin asterisco
+                        "http://localhost:5173",
+                        "http://localhost:3000"
                 )
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
