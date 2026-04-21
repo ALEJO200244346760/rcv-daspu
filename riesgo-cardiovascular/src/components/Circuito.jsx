f050b56ad119cd8201bf0e5b238a083a615c1f60
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2026-04-21 17:47:25 -0300

    casifinal6

--- riesgo-cardiovascular/src/components/Circuito.jsx
+++ riesgo-cardiovascular/src/components/Circuito.jsx
@@ -107,7 +107,7 @@ function Circuito() {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" /></svg>
               </button>
               <button onClick={() => setPacienteSeleccionado(paciente)} className="flex-1 bg-gray-100 text-gray-700 font-bold py-2 rounded-lg hover:bg-gray-200">Detalles</button>
-              <button onClick={() => irAFormularioRCV(paciente)} className="flex-1 bg-blue-600 text-white font-bold py-2 rounded-lg hover:bg-blue-700 shadow-md">Evaluar</button>
+              <button onClick={() => irAFormularioRCV(paciente)} className="flex-1 bg-red-600 text-white font-bold py-2 rounded-lg hover:bg-red-700 shadow-md">Calcular RCV</button>
             </div>
           </div>
         ))}
